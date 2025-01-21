7a5744af4f95aef050d1a38d0c136d6b0321f566
unknown <coolboyanish1123@gmail.com>
2025-01-21 12:52:12 +0545

    user controller init ,ad status column

---
+++ backend/app/Http/Controllers/Api/V1/UserController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    public function index()
+    {
+
+    }
+
+    public function show()
+    {
+
+    }
+
+    public function store()
+    {
+        
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function destroy()
+    {
+        
+    }
+}
--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('status')->comment("0:inactive/suspended  1:active");
             $table->rememberToken();
             $table->timestamps();
         });
